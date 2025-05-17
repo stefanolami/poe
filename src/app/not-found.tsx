ea55d34745400765d4f9edbd277ef5f3d67754a4
stefanolami <stefanolami90@gmail.com>
2025-05-17 17:53:08 +0300

    feat: add error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,16 @@
 'use client'
 
+import Header from '@/components/header/header'
+
 export default function NotFound() {
 	return (
-		<html>
-			<body>
-				<h1 className="text-center pt-28 text-2xl">
+		<>
+			<Header />
+			<main className="w-full h-screen flex items-center justify-center">
+				<h1 className="font-jose text-center text-2xl lg:text-4xl text-primary">
 					404 - Page not found
 				</h1>
-			</body>
-		</html>
+			</main>
+		</>
 	)
 }
