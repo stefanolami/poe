1b7eb5e064bec93b58cf9f71c6d990f05955d98d
stefanolami <stefanolami90@gmail.com>
2025-06-23 15:17:10 +0300

    feat: add dashboard block

--- src/components/admin/dashboard/dashboard.tsx
+++ src/components/admin/dashboard/dashboard.tsx
@@ -1,9 +1,23 @@
+import { ChartAreaInteractive } from '@/components/ui/dashboard/chart-area-interactive'
+import { DataTable } from '@/components/ui/dashboard/data-table'
+import { SectionCards } from '@/components/ui/dashboard/section-cards'
 import React from 'react'
 
 const DashboardComponent = () => {
 	return (
 		<div className="bg-primary text-white">
 			<h1 className="text-white font-jose text-2xl">Dashboard</h1>
+			<div className="flex flex-1 flex-col">
+				<div className="flex flex-1 flex-col gap-2">
+					<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+						<SectionCards />
+						<div className="px-4 lg:px-6">
+							<ChartAreaInteractive />
+						</div>
+						{/*  <DataTable data={data} /> */}
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
